c4e2a2fd3b9d13df3ac9334408be6abd5d14bb0a
Ana Gabriela <anagabrielaguz26@gmail.com>
2022-08-24 13:22:17 -0300

    .

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,7 +7,10 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: '#3949AB'
+        primary: '#5C6BC0',
+        secondary: '#7986CB',
+        accent: '#82B1FF',
+        error: '#FF5252',
       }
     }
   }
